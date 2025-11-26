bf9c853bbadcab6b1d6403f80e92d9caa4a5c8e4
ejas1926@gmail.com <ejas1926@gmail.com>
2025-11-26 09:09:36 +0530

    added bottomnavigation

--- lib/views/home/screen_home.dart
+++ lib/views/home/screen_home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:noteapplication/views/home/widgets/bottom_navigation.dart';
 
 class ScreenHome extends StatelessWidget {
   const ScreenHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(bottomNavigationBar: BottomNavigationMoneyManagement());
   }
 }
