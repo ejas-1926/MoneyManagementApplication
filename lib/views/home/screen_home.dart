65ecd3a8c8c1ccf2464ee90ea52e3db4b5b3e2c3
ejas1926@gmail.com <ejas1926@gmail.com>
2025-11-26 23:23:15 +0530

    added pages with switching between the bottomnaviagation

--- lib/views/home/screen_home.dart
+++ lib/views/home/screen_home.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:noteapplication/views/category/screen_category.dart';
 import 'package:noteapplication/views/home/widgets/bottom_navigation.dart';
+import 'package:noteapplication/views/transactions/screen_transaction.dart';
 
 class ScreenHome extends StatelessWidget {
-  const ScreenHome({super.key});
-
+  ScreenHome({super.key});
+  static ValueNotifier<int> selectedIndexNotifier = ValueNotifier(0);
+  final _pages = [ScreenTransaction(), ScreenCategory()];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(bottomNavigationBar: BottomNavigationMoneyManagement());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Money Manager"),
+        centerTitle: true,
+        backgroundColor: Colors.pink,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          if (selectedIndexNotifier.value == 1) {
+            print("add category");
+          } else if (selectedIndexNotifier.value == 0) {
+            print("Add transactions");
+          }
+        },
+      ),
+      bottomNavigationBar: BottomNavigationMoneyManagement(),
+      body: SafeArea(
+        child: ValueListenableBuilder(
+          valueListenable: selectedIndexNotifier,
+          builder: (context, updatedIndex, child) {
+            return _pages[updatedIndex];
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/views/home/widgets/bottom_navigation.dart
+++ lib/views/home/widgets/bottom_navigation.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:noteapplication/views/home/screen_home.dart';
 
 class BottomNavigationMoneyManagement extends StatelessWidget {
   const BottomNavigationMoneyManagement({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Transactions"),
-        BottomNavigationBarItem(icon: Icon(Icons.category), label: "category"),
-      ],
+    return ValueListenableBuilder(
+      valueListenable: ScreenHome.selectedIndexNotifier,
+      builder: (context, updatedIndex, child) {
+        return BottomNavigationBar(
+          currentIndex: updatedIndex,
+          onTap: (index) {
+            ScreenHome.selectedIndexNotifier.value = index;
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Transactions",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: "category",
+            ),
+          ],
+        );
+      },
     );
   }
 }
